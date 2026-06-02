9480d06744eed5e8cba2a00437ea2ca59818a2bc
pi2376327 <82651004+pi2376327@users.noreply.github.com>
2026-06-02 21:14:13 +0800

    Update openvpn1-up.sh

--- files/etc/openvpn/openvpn1-up.sh
+++ files/etc/openvpn/openvpn1-up.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+DATE=`date +%Y-%m-%d-%H:%M:%S`
 
 #check and add rules of mangle     
 iptables -t mangle -C PREROUTING -s 192.168.151.0/24 -m set ! --match-set chnroute dst -j MARK --set-mark 10
@@ -8,3 +9,6 @@ fi
 
 ip rule add from all fwmark 10 table 101 pref 32000
 ip route add default via 172.19.0.1 dev tun1 src 172.19.1.4 table 101
+
+echo "$DATE: Openvpn1-client startup" >>/root/script/ovpn-script.log
+echo "----------------------------------------------------------------" >>/root/script/ovpn-script.log
