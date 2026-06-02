3f502dc4c846233ce168fe571d74ceb03e9a4100
pi2376327 <82651004+pi2376327@users.noreply.github.com>
2026-06-02 20:58:58 +0800

    Create openvpn1-up.sh

---
+++ files/etc/openvpn/openvpn1-up.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#check and add rules of mangle     
+iptables -t mangle -C PREROUTING -s 192.168.151.0/24 -m set ! --match-set chnroute dst -j MARK --set-mark 10
+if [ $? = 1 ]; then
+        iptables -t mangle -I PREROUTING -s 192.168.151.0/24 -m set ! --match-set chnroute dst -j MARK --set-mark 10
+fi
+
+ip rule add from all fwmark 10 table 101 pref 32000
+ip route add default via 172.19.0.1 dev tun1 src 172.19.1.4 table 101
