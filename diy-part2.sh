df661aeb100c222b2a9a137333f67e6d12289f30
pi2376327 <82651004+pi2376327@users.noreply.github.com>
2026-06-26 11:54:30 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -26,4 +26,4 @@ git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-a
 git clone https://github.com/pi2376327/openwrt-redsocks2.git package/redsocks2
 
 #download binary NextTrace
-wget -O files/usr/bin/nexttrace https://github.com/nxtrace/NTrace-core/releases/download/v1.7.1/nexttrace_linux_arm64 && chmod +x /usr/bin/nexttrace 
+#wget -O files/usr/bin/nexttrace https://github.com/nxtrace/NTrace-core/releases/download/v1.7.1/nexttrace_linux_arm64 && chmod +x /usr/bin/nexttrace 
